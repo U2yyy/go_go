2b6d9e2d84cd7869f17bda4d4345697fc507a715
U2yyy <a1535887812@gmail.com>
2022-09-27 22:00:26 +0800

    feat: 静态资源初始化

---
+++ go-go/src/components/ClearBox.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "ClearBox"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ go-go/src/components/Game.vue
@@ -0,0 +1,37 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Game",
+  data(){
+    return {
+      imgItem:[
+        "./img/ak47.jpeg",
+        "./img/awp.jpeg",
+        "./img/eagle.jpeg",
+        "./img/m4a1.jpeg",
+        "./img/p250.jpeg",
+        "./img/r8.jpeg",
+        "./img/usp.jpeg"
+      ],
+      HEIGHT: 50,
+      WIDTH: 50,
+      ItemHeight: 8,
+      ItemWidth: 5
+    }
+  },
+  methods:{
+    createItem(src){
+      
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
