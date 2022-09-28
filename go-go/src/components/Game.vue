6d6c09cf68cd0bdbecb63d46522567947c806abf
u2yyy <a1535887812@gmail.com>
2022-09-28 21:13:38 +0800

    feat: 一些页面布局

--- go-go/src/components/Game.vue
+++ go-go/src/components/Game.vue
@@ -1,25 +1,28 @@
 <template>
+  <div class="background-box">
+    <div class="game-zone">
+      <div class="card" v-for="item in card" :key="item.id" :style="{left: item.left,top: item.top}">
 
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import nanoid from 'nano-id';
 export default {
   name: "Game",
   data(){
     return {
-      imgItem:[
-        "./img/ak47.jpeg",
-        "./img/awp.jpeg",
-        "./img/eagle.jpeg",
-        "./img/m4a1.jpeg",
-        "./img/p250.jpeg",
-        "./img/r8.jpeg",
-        "./img/usp.jpeg"
+      card:[
+        {id:nanoid(),img:"../img/ak47.jpeg",name:"ak47"},
+        {id:nanoid(),img:"../img/awp.jpeg",name:"awp"},
+        {id:nanoid(),img:"../img/eagle.jpeg",name:"eagle"},
+        {id:nanoid(),img:"../img/m4a1.jpeg",name:"m4a1"},
+        {id:nanoid(),img:"../img/p250.jpeg",name:"p250"},
+        {id:nanoid(),img:"../img/r8.jpeg",name:"r8"},
+        {id:nanoid(),img:"../img/usp.jpeg",name:"usp"},
       ],
-      HEIGHT: 50,
-      WIDTH: 50,
-      ItemHeight: 8,
-      ItemWidth: 5
     }
   },
   methods:{
@@ -33,5 +36,16 @@ export default {
 </script>
 
 <style scoped>
-
+  .background-box {
+    width: 100%;
+    height: 40rem;
+    background: aqua;
+    marin: 0 auto;
+  }
+  .game-zone {
+    width: 30rem;
+    height: 30rem;
+    background: antiquewhite;
+    margin: 0 auto;
+  }
 </style>
