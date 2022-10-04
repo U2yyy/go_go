25af7ba543142c7a934bacee31d7873bc0ce5437
U2yyy <a1535887812@gmail.com>
2022-10-04 22:52:58 +0800

    增加了一些逻辑和棋盘

--- go-go/src/components/Game.vue
+++ go-go/src/components/Game.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="background-box">
     <div class="game-zone">
-      <div class="card" v-for="item in card" :key="item.id" :style="{left: item.left,top: item.top}">
-        <img :src="item.img">
+      <div class="card" v-for="item in table" :key="item.id" :style="{left: item.left,top: item.top}">
+        {{item.id}}
+        <img :src="item.imgSrc" alt="">
       </div>
     </div>
     <audio ref="GoAudio" :src="GoAudio" loop autoplay></audio>
@@ -12,7 +13,7 @@
           v-if="buttonExist"
           class="begin-button"
           type="primary"
-          @click.once="AudioOn"
+          @click.once="GameStart"
       >Start</el-button>
     </transition>
   </div>
@@ -27,41 +28,101 @@ import p250 from "../assets/img/p250.png";
 import r8 from "../assets/img/r8.png";
 import usp from "../assets/img/usp.png";
 import GoAudio from "../assets/audio/work-hard,play-hard.mp3";
-import nanoid from 'nano-id';
 import { ElButton } from 'element-plus';
 import 'animate.css';
+
+const CardWidth = 3;
+const CardHeight = 4;
 
 export default {
   name: "Game",
   components: { ElButton },
   data(){
     return {
-      card:[
-        {id:nanoid(),img:ak47,name:"ak47"},
-        {id:nanoid(),img:awp,name:"awp"},
-        {id:nanoid(),img:eagle,name:"eagle"},
-        {id:nanoid(),img:m4a1,name:"m4a1"},
-        {id:nanoid(),img:p250,name:"p250"},
-        {id:nanoid(),img:r8,name:"r8"},
-        {id:nanoid(),img:usp,name:"usp"},
-      ],
+      img:[ak47,awp,eagle,m4a1,p250,r8,usp],
+      table:[],
+      cards:[],
+      selectedCards:[],
       GoAudio,
-      buttonExist:true
+      buttonExist:true,
     }
   },methods:{
     AudioOn(){
       this.$refs.GoAudio.play();
       this.buttonExist = false;
+    },
+    initTable(){
+      let k = 1;
+      let left = 0;
+      let top = 0;
+      for(let i=1;i<=10;i++){
+        left = 0;
+        for(let j=1;j<=10;j++){
+          let id = (i-1)*10 + j;
+          let isExist = false;
+          let isCovered = false;
+          let index = k;
+          let imgSrc ='';
+          this.table.push({id,index,isCovered,isExist,imgSrc,left:left+'rem',top:top+'rem'});
+          left += CardWidth;
+        }
+      top += CardHeight;
+      }
+      left = 1.5;
+      top = 2;
+      for(let i=1;i<=9;i++){
+        left = 1.5;
+        for(let j=1;j<=9;j++){
+          let id = (i-1)*10 + j;
+          let isExist = false;
+          let isCovered = false;
+          let index = k;
+          let imgSrc ='';
+          this.table.push({id,index,isCovered,isExist,imgSrc,left:left+'rem',top:top+'rem'});
+          left += CardWidth;
+        }
+        top += CardHeight;
+      }
+    },
+    initCard: function (num) {
+      while (num >= 0) {
+        for (let n = 0; n < 3; n++) {
+          let imgNum = Math.floor(Math.random() * 8), imgSrc = this.img[imgNum], i = Math.floor(Math.random() * 2) + 1,
+              j, tar;
+          do {
+            if (i % 2 === 0) {
+              j = Math.floor(Math.random() * 81) + 1;
+            } else {
+              j = Math.floor(Math.random() * 100) + 1;
+            }
+            tar = this.table.find(({id, index}) => {
+              return (id === j && index === i);
+            })
+            console.log(tar);
+          } while (tar.isExist)
+          tar.isExist = true;
+          tar.imgSrc = imgSrc;
+        }
+        num--;
+      }
+    },
+    GameStart(){
+      let num = 50;
+      this.initCard(num);
+      this.AudioOn();
     }
+  },
+  mounted() {
+    this.initTable();
   }
-/*  这个方法自动循环播放不好使
-    methods:{
-    AudioOn(){
-      let musicGo = new Audio(GoAudio);
-      musicGo.onload;
-      musicGo.play();
-    }
-  }*/
+  /*  这个方法自动循环播放不好使
+      methods:{
+      AudioOn(){
+        let musicGo = new Audio(GoAudio);
+        musicGo.onload;
+        musicGo.play();
+      }
+    }*/
   /*不能直接用定时器去自动播放音频
   mounted() {
     setTimeout(()=>{
@@ -87,6 +148,7 @@ export default {
     align-items:center;
   }
   .game-zone {
+    position: relative;
     width: 30rem;
     height: 40rem;
     background-color:transparent;
@@ -94,11 +156,12 @@ export default {
     border-radius: 2rem;
   }
   .card {
+    position: absolute;
     width: 3rem;
     height: 4rem;
     border-radius: 0.2rem;
     border: pink solid 0.1rem;
-    background: gray;
+    background: transparent;
   }
   .begin-button{
     position: absolute;
